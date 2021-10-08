7eb9f5985e0f81e37efe698d005d9ee561b3f760
baharalidurrani <baharalidurrani@gmail.com>
2021-10-08 14:37:19 +0500

    graphql-codegen added to auto generate hooks

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,26 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import { useFindAllUsersQuery } from "./generated/graphql";
 
 function App() {
+  const { data, error, loading } = useFindAllUsersQuery();
   return (
     <div className="App">
+      {error ? (
+        error.message
+      ) : (
+        <>
+          <h1>
+            Id:
+            {loading ? "Loading..." : data?.findAllUsers[0].id}
+          </h1>
+          <h1>
+            Name:
+            {loading ? "Loading..." : data?.findAllUsers[0].name}
+          </h1>
+        </>
+      )}
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
---
+++ src/generated/graphql.ts
@@ -0,0 +1,106 @@
+import { gql } from '@apollo/client';
+import * as Apollo from '@apollo/client';
+export type Maybe<T> = T | null;
+export type Exact<T extends { [key: string]: unknown }> = { [K in keyof T]: T[K] };
+export type MakeOptional<T, K extends keyof T> = Omit<T, K> & { [SubKey in K]?: Maybe<T[SubKey]> };
+export type MakeMaybe<T, K extends keyof T> = Omit<T, K> & { [SubKey in K]: Maybe<T[SubKey]> };
+const defaultOptions =  {}
+/** All built-in and custom scalars, mapped to their actual values */
+export type Scalars = {
+  ID: string;
+  String: string;
+  Boolean: boolean;
+  Int: number;
+  Float: number;
+};
+
+export type CreateUserInput = {
+  name: Scalars['String'];
+};
+
+export type Mutation = {
+  __typename?: 'Mutation';
+  createUser: User;
+  removeUser: User;
+  updateUser: User;
+};
+
+
+export type MutationCreateUserArgs = {
+  createUserInput: CreateUserInput;
+};
+
+
+export type MutationRemoveUserArgs = {
+  id: Scalars['Int'];
+};
+
+
+export type MutationUpdateUserArgs = {
+  updateUserInput: UpdateUserInput;
+};
+
+export type Query = {
+  __typename?: 'Query';
+  findAllUsers: Array<User>;
+  findOne: User;
+};
+
+
+export type QueryFindOneArgs = {
+  id: Scalars['Int'];
+};
+
+export type UpdateUserInput = {
+  id: Scalars['Int'];
+  name?: Maybe<Scalars['String']>;
+};
+
+export type User = {
+  __typename?: 'User';
+  /** Example field (placeholder) */
+  id: Scalars['Float'];
+  name: Scalars['String'];
+};
+
+export type FindAllUsersQueryVariables = Exact<{ [key: string]: never; }>;
+
+
+export type FindAllUsersQuery = { __typename?: 'Query', findAllUsers: Array<{ __typename?: 'User', id: number, name: string }> };
+
+
+export const FindAllUsersDocument = gql`
+    query findAllUsers {
+  findAllUsers {
+    id
+    name
+  }
+}
+    `;
+
+/**
+ * __useFindAllUsersQuery__
+ *
+ * To run a query within a React component, call `useFindAllUsersQuery` and pass it any options that fit your needs.
+ * When your component renders, `useFindAllUsersQuery` returns an object from Apollo Client that contains loading, error, and data properties
+ * you can use to render your UI.
+ *
+ * @param baseOptions options that will be passed into the query, supported options are listed on: https://www.apollographql.com/docs/react/api/react-hooks/#options;
+ *
+ * @example
+ * const { data, loading, error } = useFindAllUsersQuery({
+ *   variables: {
+ *   },
+ * });
+ */
+export function useFindAllUsersQuery(baseOptions?: Apollo.QueryHookOptions<FindAllUsersQuery, FindAllUsersQueryVariables>) {
+        const options = {...defaultOptions, ...baseOptions}
+        return Apollo.useQuery<FindAllUsersQuery, FindAllUsersQueryVariables>(FindAllUsersDocument, options);
+      }
+export function useFindAllUsersLazyQuery(baseOptions?: Apollo.LazyQueryHookOptions<FindAllUsersQuery, FindAllUsersQueryVariables>) {
+          const options = {...defaultOptions, ...baseOptions}
+          return Apollo.useLazyQuery<FindAllUsersQuery, FindAllUsersQueryVariables>(FindAllUsersDocument, options);
+        }
+export type FindAllUsersQueryHookResult = ReturnType<typeof useFindAllUsersQuery>;
+export type FindAllUsersLazyQueryHookResult = ReturnType<typeof useFindAllUsersLazyQuery>;
+export type FindAllUsersQueryResult = Apollo.QueryResult<FindAllUsersQuery, FindAllUsersQueryVariables>;
